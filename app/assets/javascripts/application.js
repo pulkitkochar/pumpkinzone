3660603de8e5e5f8d02dd8bf225d2d807b057c7d
Pulkit <pulkitkochar1991@gmail.com>
2017-06-24 03:08:16 +0530

    adding carousals on home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+    $(".carousel-inner").swiperight(function() {
+        $(this).parent().carousel('prev');
+    });
+    $(".carousel-inner").swipeleft(function() {
+        $(this).parent().carousel('next');
+    });
+});
