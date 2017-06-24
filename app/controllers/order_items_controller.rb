956c7301d59621dfbbc878f815ba6315b4080335
Shilpa Singla <shilpasingla@inshilpas.local>
2017-06-24 18:18:48 +0530

    updates/deletes from checkout

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -17,6 +17,7 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.update_attributes(order_item_params)
     @order_items = @order.order_items
+    redirect_to :back
   end
 
   def destroy
@@ -24,6 +25,7 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.destroy
     @order_items = @order.order_items
+    redirect_to :back
   end
   private
 
