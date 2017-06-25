d5c7dbc1ada0be6f36603826b663b65cfe8090d9
Pulkit <pulkitkochar1991@gmail.com>
2017-06-25 23:52:16 +0530

    modifying products page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ Category.create! id: 4, name: 'Induction Cookware'
 Category.create! id: 5, name: 'Anodized Cookware'
 
 Product.delete_all
-Product.create! id: 1, category_id: 3, name: 'PUMPKIN KADAHI WITH SS LID (HARD ANODISED) WITH INDUCTION BASE 1.50 LTR', price: 649, active: true
+Product.create! id: 1, category_id: 3, name: 'PUMPKIN KADAHI WITH SS LID', price: 649, active: true
 Product.create! id: 2, category_id: 1,name: "Pumpkin Induction Pressure Cooker", price: 0.29, active: true
 Product.create! id: 3, category_id: 1,name: "Pumpkin Hard Anodized Pressure Cooker", price: 1.99, active: true
 Product.create! id: 4, category_id: 1,name: "Pumpkin Regular Pressure Cooker", price: 1.99, active: true
