8eb6393f0836449728af42dc152185ab3e86675c
Pulkit <pulkitkochar1991@gmail.com>
2017-06-29 01:02:02 +0530

    adding functionality for side nav bar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @categories = Category.all
+    @products_with_categories = []
+    @categories.each do |category|
+      @products_with_categories << { category => Product.where(category_id: category.id)}
+    end
     @order = current_order
     @order_item = current_order.order_items.new
   end
